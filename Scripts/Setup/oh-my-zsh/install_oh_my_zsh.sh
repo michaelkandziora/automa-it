6d4cdabbb632fd6ad7d824924ce1f66fd5fa79b2
Michael Kandziora <dev@kandziora.me>
2024-05-20 00:01:23 +0200

    hotfix(omz): added some feedback for default shell zsh and backup .zshrc before overwrite

--- Scripts/Setup/oh-my-zsh/install_oh_my_zsh.sh
+++ Scripts/Setup/oh-my-zsh/install_oh_my_zsh.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
+# Debugging aktivieren
+set -e
+
 # Importiere Hilfsfunktionen für Konfigurationsmanagement
-source ./utils.sh
-echo $(pwd) # show me current working directory, I guess script can't find ./utils.sh file
+# Sucht nach der Datei 'utils.sh' ab dem Wurzelverzeichnis des Projekts
+# Start im aktuellen Verzeichnis
+dir="."
+
+# Loop, um nach oben im Verzeichnisbaum zu gehen
+while : ; do
+    # Suche nach der utils.sh im aktuellen Verzeichnis
+    file_path=$(find "$dir" -maxdepth 1 -type f -name "utils.sh" -print -quit)
+    
+    # Prüfen, ob die Datei gefunden wurde
+    if [[ -n $file_path ]]; then
+        source "$file_path"
+        echo "Datei gefunden und gesourced: $file_path"
+        break
+    fi
+
+    # Abbruchbedingungen: root oder temp directory erreicht
+    if [[ "$dir" == "/" || "$dir" =~ ^/tmp/tmp\.* ]]; then
+        echo "utils.sh nicht gefunden. Suchbereich endete bei: $dir"
+        break
+    fi
+
+    # Gehe ein Verzeichnis höher
+    dir=$(dirname "$dir")
+done
+echo "Aktuelles Verzeichnis: $(pwd)" # show me current working directory, I guess script can't find ./utils.sh file
 
 # Überprüfen der benötigten Befehle
 required_commands=("curl" "git" "zsh")
@@ -47,13 +74,14 @@ if [[ -f "$HOME/.zshrc" ]]; then
 fi
 
 # Oh My Zsh Installation starten
-echo -e "${GREEN}Beginne mit der Installation von Oh My Zsh...${NC}"
+echo "Beginne mit der Installation von Oh My Zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-echo -e "${GREEN}Oh My Zsh wurde erfolgreich installiert.${NC}"
+echo "Oh My Zsh wurde erfolgreich installiert."
 
-# Starte die Konfiguration
-# customize_omz.sh  # Annahme: Dieses Skript wird später erstellt und angepasst.
-
-# Make zsh default Shell
-echo "/usr/bin/zsh" | sudo tee -a /etc/shells
+# Zsh zur Liste der erlaubten Shells hinzufügen und als Standardshell setzen
+if ! grep -q "/usr/bin/zsh" /etc/shells; then
+    echo "/usr/bin/zsh" | sudo tee -a /etc/shells
+fi
 chsh -s $(which zsh)
+
+echo "Zsh wurde als Standardshell gesetzt. Bitte melden Sie sich ab und wieder an, um die Änderungen zu übernehmen."
