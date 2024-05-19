38e8bbe4552e94d8de8799b1c44f2c6bc7194334
Michael Kandziora <dev@kandziora.me>
2024-05-20 01:17:27 +0200

    feat(core): added quick setup

---
+++ Scripts/Setup/Quick/setup-basics.sh
@@ -0,0 +1,10 @@
+bash ./Git/install_git.sh --silent
+bash ./Python/setup_python.sh --silent
+
+bash ./oh-my-zsh/install_oh_my_zsh.sh --silent
+bash ./oh-my-zsh/customize_oh_my_zsh.sh --silent
+
+bash ./Vim/setup_vim.sh --silent
+
+bash ./Docker/install_docker.sh --silent
+bash ./Docker/fix-docker-gpg.sh --silent
